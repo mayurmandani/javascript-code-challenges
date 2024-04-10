6a70a0022079c6024a193651920a52bbacd3450a
Mayur Mandani <mmandani9@gmail.com>
2024-04-10 17:40:48 +0530

    ShallowCopy changes are updated

--- ShallowCopy.js
+++ ShallowCopy.js
@@ -3,14 +3,21 @@ A shallow copy is a copy of an object whose references are the same as the origi
 This means that if you change the value of a property in the shallow copy, it will also change the value of the property in the original object.
 */
 
-const user = {
-    name: "John",
-    age: 32,
-    job: "React Developer"
-}
- const cloneShallowCopy = user;
+// Origional object
+const person = {
+    name: 'Mayur',
+    age: 26,
+    address: {
+        city: 'Vadodara',
+        country: 'India'
+    } 
+};
 
-cloneShallowCopy.job = "Frontend Developer";
+// Shallow Copy using Object.assign()
+const shallowCopy = Object.assign({}, person);
 
-console.log(user, "original object");
-console.log(cloneShallowCopy, "references object");
+shallowCopy.address.city = "Surat";
+
+console.log(person.address.city); // Origional object person address city value changed
+console.log(shallowCopy.address.city); 
+
