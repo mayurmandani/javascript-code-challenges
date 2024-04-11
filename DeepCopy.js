a11b12c2aa90697b33ddd1b94b6b72012b99d143
Mayur Mandani <mmandani9@gmail.com>
2024-04-11 09:31:27 +0530

    DeepCopy changes are updated

--- DeepCopy.js
+++ DeepCopy.js
@@ -3,11 +3,20 @@ A deep copy is a copy of an object whose references are not the same as the orig
 This means that if you change the value of a property in the deep copy, it will not change the value of the property in the original object. 
 */
 
-const originalObject = { name: 'Mayur', age: 32 };
+// Origional object
+const person = {
+    name: 'Mayur',
+    age: 26,
+    address: {
+        city: 'Vadodara',
+        country: 'India'
+    } 
+};
 
-const cloneDeepCopy = JSON.parse(JSON.stringify(originalObject));
+// Deep copy using JSON.parse() and JSON.stringify()
+const deepCopy = JSON.parse(JSON.stringify(person));
 
-cloneDeepCopy.name = "Rahul";
+deepCopy.address.city = "Surat";
 
-console.log(originalObject, "original object");
-console.log(cloneDeepCopy, "references object");
+console.log(person.address.city); // Origional object person address city value not changed and keep it Original object
+console.log(deepCopy.address.city); // As Expected deepCopy object address city value changed to Surat 
