6022d8f6cec88c536eb84ca811966d05200adc3a
Mayur Mandani <mmandani9@gmail.com>
2024-04-28 15:02:43 +0530

    JavascriptPuzzle2 changes are committed

---
+++ JavascriptPuzzle2.js
@@ -0,0 +1,10 @@
+// What is the output of the following code snippet and why ?
+
+const str = "script";
+
+const paddStr = str.padStart(10, "java-");
+
+console.log(paddStr);
+
+// Pad start will add beginning characters accroding to position passed in the first parameter.
+
