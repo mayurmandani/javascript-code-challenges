8802b58d9e587ace1ad7fd17155c587fa0a436a0
Mayur Mandani <mmandani9@gmail.com>
2024-05-29 14:28:49 +0530

    ChaningMethod changes are committed

---
+++ ChaningMethod.js
@@ -0,0 +1,7 @@
+// use Javascript chaining method concept and convert input " hello " to  output as "HELLO";
+
+let input = " hello  ";
+
+let output = input.toUpperCase().trim();
+
+console.log(output);
