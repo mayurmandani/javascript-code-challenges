cb1a061b40c24e5c04e1858b53178d63a633953a
Mayur Mandani <mmandani9@gmail.com>
2024-06-09 18:39:28 +0530

    JavascriptPuzzle8 changes are committed

---
+++ JavascriptPuzzle8.js
@@ -0,0 +1,13 @@
+let numbers = [1, 2, 3, 4, 5];
+
+const mapNumbers = numbers.map(e => e > 3);
+
+console.log(mapNumbers);
+
+const filterNumbers = numbers.filter(e => e > 3);
+
+console.log(filterNumbers);
+
+const findNumbers= numbers.find(e => e > 3);
+
+console.log(findNumbers);
