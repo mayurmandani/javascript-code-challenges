24f70f22a972c740d1e3ab160c42114c1f86fd05
Mayur Mandani <mmandani9@gmail.com>
2024-06-29 12:52:14 +0530

    Object different way creation changes are committed

--- JavascriptPuzzle14.js
+++ JavascriptPuzzle14.js
@@ -11,3 +11,19 @@ men.age = 28;
 men.role = "Trainer";
 
 console.log(men);
+
+// Object literal
+const personDetails = {
+   name: "Nitin",
+   age: 38,
+   role: "Tester"
+};
+
+console.log(personDetails);
+
+// Object Constructor
+let cutomer = new Object();
+cutomer.name = "ABC Info";
+cutomer.city = "Ahmedabad";
+cutomer.owner = "Mihir Mehta";
+console.log(cutomer);
