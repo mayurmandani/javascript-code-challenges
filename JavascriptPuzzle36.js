a6b03b99a73d8f7b08d409f62981775c3f15b9aa
Mayur Mandani <mmandani9@gmail.com>
2024-08-20 13:00:41 +0530

    JavascriptPuzzle36 changes are committed

---
+++ JavascriptPuzzle36.js
@@ -0,0 +1,8 @@
+// How to get the Last Element of an Array
+
+let array = [1, 2, 3, 4, 9];
+
+// Get the last element using length property
+let lastElement = array[array.length - 1];
+
+console.log("Last element of the array:", lastElement);
