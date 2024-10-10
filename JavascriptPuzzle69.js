4dc04a07f39c46d72a7c4be7d7feb8e4c8bcd0de
Mayur Mandani <mmandani9@gmail.com>
2024-10-10 11:35:21 +0530

    JavascriptPuzzle69 changes are committed

---
+++ JavascriptPuzzle69.js
@@ -0,0 +1,5 @@
+/* What is the output of the below code */
+
+console.log('hello' - 'world');
+
+// NaN - Subtracting strings is not a valid operation in JavaScript, resulting in NaN (Not a Number).
