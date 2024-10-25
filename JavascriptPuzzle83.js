7a7ff8d2c5d9ffa94bf9de7285f03328d55758e6
Mayur Mandani <mmandani9@gmail.com>
2024-10-25 11:34:26 +0530

    JavascriptPuzzle83 changes are committed

---
+++ JavascriptPuzzle83.js
@@ -0,0 +1,32 @@
+/* Write a javaScript function for memoization with example */
+
+const memoize = (fn) => {
+   const cache = {}
+    
+   return (...args) => {
+        const argsToString = JSON.stringify(args)
+        if (argsToString in cache) {
+            console.log(`fetching from cache for key ${argsToString}`)
+            return cache[argsToString]
+        } else {
+             console.log(`computing values for args ${argsToString}`)
+            const result = fn.apply(this, args);
+            cache[argsToString] = result
+            return result
+        }
+   }
+}
+
+const addThreeNums = (a, b, c) => a + b + c
+const add = memoize(addThreeNums);
+console.log(add(1, 2, 3));
+console.log(add(1, 2, 3));
+
+/* second example */
+const factorial = memoize((x) => {
+    if(x === 0) return 1
+    else return x * factorial(x - 1)
+});
+
+console.log(factorial(5));
+console.log(factorial(6));
