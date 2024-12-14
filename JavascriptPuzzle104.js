15f10be4c0176e8374a61e485e4fefed073729d9
Mayur Mandani <mmandani9@gmail.com>
2024-12-14 20:12:34 +0530

    JavascriptPuzzle104 changes are committed

---
+++ JavascriptPuzzle104.js
@@ -0,0 +1,15 @@
+// number to string conversion
+
+const num = 12;
+
+console.log(String(num));
+console.log(num.toString());
+console.log(num + "");
+
+// string to number conversion
+
+const str = "11";
+
+console.log(Number(str));
+console.log(+str);
+console.log(parseInt(str));
