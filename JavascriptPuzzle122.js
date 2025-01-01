7d41bc2009ef23896219455bc5fd260f9d6beb16
Mayur Mandani <mmandani9@gmail.com>
2025-01-01 11:26:10 +0530

    JavascriptPuzzle122 changes are committed

---
+++ JavascriptPuzzle122.js
@@ -0,0 +1,9 @@
+/* Assign the values of the array pixel to three variables red, green and blue using destructuring */
+
+let pixel = [100, 120, 215];
+
+let [red, green, blue] = pixel;
+
+console.log(red);
+console.log(green);
+console.log(blue);
