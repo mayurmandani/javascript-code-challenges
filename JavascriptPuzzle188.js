f2b596eb37661cf87d89d1a3ce47149c89711de8
Mayur Mandani <mmandani9@gmail.com>
2025-03-05 21:53:34 +0530

    JavascriptPuzzle188 changes are committed

---
+++ JavascriptPuzzle188.js
@@ -0,0 +1,32 @@
+/* What will be the output of the following code? */
+
+function asyncFunc() {
+
+  console.log("Started asyncFunc1");
+  //Async1 code
+  setTimeout(() => {
+    console.log("Completed asyncFunc1");
+    console.log("Started asyncFunc2");
+    //Async2 code
+    setTimeout(() => {
+      console.log("Completed asyncFunc2");
+      console.log("Started asyncFunc3");
+      //Async3 function code
+      setTimeout(() => {
+        console.log("Completed asyncFunc3");
+      }, 1000);
+    }, 2000);
+  }, 3000);
+
+}
+
+asyncFunc();
+
+/* 
+Started asyncFunc1
+Completed asyncFunc1
+Started asyncFunc2
+Completed asyncFunc2
+Started asyncFunc3
+Completed asyncFunc3
+*/
