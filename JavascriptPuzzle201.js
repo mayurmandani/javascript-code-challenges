2d4baab6eb9996d3aa516f4442597bd1d985cf2c
Mayur Mandani <mmandani9@gmail.com>
2025-03-09 21:30:58 +0530

    JavascriptPuzzle201 changes are committed

---
+++ JavascriptPuzzle201.js
@@ -0,0 +1,11 @@
+/* What will be the output of the following code? */
+
+function calculateSum() {
+  console.log(result);
+  var num1 = 5;
+  let num2 = 10;
+  const num3 = 15;
+  var result = num1 + num2 + num3;
+}
+
+calculateSum();  // undefined
