3f16b0548e3d0e5a81f84d65a04b2b6205602e5e
Mayur Mandani <mmandani9@gmail.com>
2025-03-10 10:15:16 +0530

    JavascriptPuzzle202 changes are committed

---
+++ JavascriptPuzzle202.js
@@ -0,0 +1,13 @@
+/* What will be the output of the following code? */
+
+function* generatorFunction() {
+  yield 1;
+  yield 2;
+  return 3;
+}
+
+const generator = generatorFunction();
+
+console.log(generator.next());  // { value: 1, done: false }
+console.log(generator.next());  // { value: 2, done: false }
+console.log(generator.next());  // { value: 3, done: true } 
