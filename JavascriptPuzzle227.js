30b8230d9e59e6bc9d0e582df336993758f2ebdb
Mayur Mandani <mmandani9@gmail.com>
2025-03-24 10:47:06 +0530

    JavascriptPuzzle227 changes are committed

---
+++ JavascriptPuzzle227.js
@@ -0,0 +1,13 @@
+/* What will be the output of the following code? */
+
+const arr = [1, 2, 3, 4, 5];
+
+arr.forEach(function (element) {
+  console.log(element);
+});
+
+// 1
+// 2
+// 3
+// 4
+// 5
