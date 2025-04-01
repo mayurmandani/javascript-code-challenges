70cfdc4f0c46a0f02232d99344d11b0f2f66cb91
Mayur Mandani <mmandani9@gmail.com>
2025-04-01 12:27:01 +0530

    JavascriptPuzzle242 changes are committed

---
+++ JavascriptPuzzle242.js
@@ -0,0 +1,7 @@
+/* What will be the output of the following code? */
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+console.log(null === undefined); // false
+console.log (null == undefined) // true
