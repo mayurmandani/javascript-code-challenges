1d19f64d845cea4b1f13100cd59d26f4485b4336
Mayur Mandani <mmandani9@gmail.com>
2025-04-09 10:18:09 +0530

    JavascriptPuzzle251 changes are committed

---
+++ JavascriptPuzzle251.js
@@ -0,0 +1,10 @@
+/* What will be the output of the following code? */
+
+console.log(typeof 42);  // number
+console.log(typeof "Hello"); // string
+console.log(typeof true); // boolean
+console.log(typeof [1, 2, 3]); // object
+console.log(typeof { name: "John", age: 25 }); // object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof function () {}); // function
