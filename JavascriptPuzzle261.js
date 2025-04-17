5c6f988ae999e008b34ae8ba8fa790f33d59543a
Mayur Mandani <mmandani9@gmail.com>
2025-04-17 15:36:35 +0530

    JavascriptPuzzle261 changes are committed

---
+++ JavascriptPuzzle261.js
@@ -0,0 +1,9 @@
+/* Write a function which accept number as argument and return the sum of all number */
+
+// using the Big - O Calculation
+
+function summation(n) {
+  return (n * (n + 1)) / 2;
+}
+
+console.log(summation(10)); // 55
