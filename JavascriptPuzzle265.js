bae7b2639768341afc40d7605af0a7c0eb30dab5
Mayur Mandani <mmandani9@gmail.com>
2025-04-21 10:48:22 +0530

    JavascriptPuzzle265 changes are committed

---
+++ JavascriptPuzzle265.js
@@ -0,0 +1,19 @@
+/* Power of Two - Give a positive number 'n', determine if the number is power of 2 or not */
+
+function isPowerOfTwo(n) {
+
+  if( n < 1) {
+    return false
+  }
+  while(n > 1) {
+    if( n % 2 !== 0) {
+      return false
+    }
+    n = n /2
+  }
+    return true
+}
+
+console.log(isPowerOfTwo(1)); // true
+console.log(isPowerOfTwo(2)); // true
+console.log(isPowerOfTwo(5)); // false
