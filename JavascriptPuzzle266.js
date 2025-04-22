222273de3302bed38cc690548f4c66a89b99239d
Mayur Mandani <mmandani9@gmail.com>
2025-04-22 10:02:46 +0530

    JavascriptPuzzle266 changes are committed

---
+++ JavascriptPuzzle266.js
@@ -0,0 +1,13 @@
+/* Recursive Fibonacci Sequence - Give a number 'n', find the nth element of the Fibonacci sequence */
+
+
+function recursiveFibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
+
+console.log(recursiveFibonacci(0)); // 0
+console.log(recursiveFibonacci(1)); // 1
+console.log(recursiveFibonacci(6)); // 8
