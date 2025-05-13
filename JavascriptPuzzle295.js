4060b3bc9de05962ef6d6bdf9bc481adad196e15
Mayur Mandani <mmandani9@gmail.com>
2025-05-13 09:34:19 +0530

    JavascriptPuzzle295 changes are committed

---
+++ JavascriptPuzzle295.js
@@ -0,0 +1,11 @@
+/* What will be the output of the below code: */
+
+const object1 = {
+  prop1: "value1",
+  prop2: {
+	prop3: "value3",
+  },
+};
+ const newObj = { ...object1 };
+newObj.prop2.prop3 = "newValue3";
+console.log(object1.prop2.prop3);  // newValue3
