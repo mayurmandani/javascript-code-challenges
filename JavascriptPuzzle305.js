2893f25bffe1d9c347fc4314c742f534894bb989
Mayur Mandani <mmandani9@gmail.com>
2025-05-23 12:19:39 +0530

    JavascriptPuzzle305 changes are committed

---
+++ JavascriptPuzzle305.js
@@ -0,0 +1,9 @@
+/* 
+toReversed() method transposes the elements of the calling array object in reverse order and returns a new array.
+*/
+
+const months = ['January', 'February', 'March', 'April', 'May'];
+
+const reverseMonths = months.toReversed();
+
+console.log(reverseMonths); [ 'May', 'April', 'March', 'February', 'January' ]
