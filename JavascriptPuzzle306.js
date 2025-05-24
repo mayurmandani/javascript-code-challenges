c36e74fe80f504012098d817130dcd55e1514ece
Mayur Mandani <mmandani9@gmail.com>
2025-05-24 10:48:18 +0530

    JavascriptPuzzle306 changes are committed

---
+++ JavascriptPuzzle306.js
@@ -0,0 +1,9 @@
+/* 
+The toSorted() method of Array instances and it returns a new array with the elements sorted in ascending order.
+*/
+
+const prime = [13,7,17,2];
+
+const sortPrime = prime.toSorted();
+
+console.log(sortPrime); // [ 13, 17, 2, 7 ]
