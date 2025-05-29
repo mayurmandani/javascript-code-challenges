fb8500b233bf82d9857214370e47954016f6260a
Mayur Mandani <mmandani9@gmail.com>
2025-05-29 12:32:21 +0530

    JavascriptPuzzle311 changes are committed

---
+++ JavascriptPuzzle311.js
@@ -0,0 +1,52 @@
+/* Queue Object Example */
+
+class Queue {
+      constructor() {
+        this.items = {}
+        this.rear = 0;
+        this.front = 0;
+      }
+
+      enqueue(element) {
+        this.items[this.rear] = element;
+        this.rear++;
+      }
+
+      dequeue() {
+        const item = this.items[this.front];
+        delete this.items[this.front];
+        this.front++;
+        return item
+      }
+
+      isEmpty() {
+        return this.rear - this. front === 0
+      }
+      peeek() {
+        return this.items[this.front];
+      }
+
+      size() {
+        return this.rear - this.front
+      }
+
+      print() {
+        console.log(this.items);
+      }
+}
+
+const queue = new Queue()
+
+console.log(queue.isEmpty());  // true
+
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+
+console.log(queue.size());  // 3
+
+queue.print();  // { '0': 10, '1': 20, '2': 30 }
+
+console.log(queue.dequeue());  // 10
+
+console.log(queue.peeek()); // 20
