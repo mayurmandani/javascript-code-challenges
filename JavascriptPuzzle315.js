2860dd668385438ff67e0812da603fb6774b975c
Mayur Mandani <mmandani9@gmail.com>
2025-06-03 12:08:37 +0530

    JavascriptPuzzle315 changes are updated

---
+++ JavascriptPuzzle315.js
@@ -0,0 +1,104 @@
+/* Linked List Insert Data Structure Example */
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+ 
+  // O(1)
+  prepend(value) {
+      const node = new Node(value);
+      if(this.isEmpty()) {
+        this.head = node
+      } else {
+        node.next = this.head;
+        this.head = node
+      }
+      this.size++;
+  }
+
+  // O(n)
+  append(value) { 
+    const node = new Node(value);
+    if(this.isEmpty()) {
+      this.head = node
+    } else {
+       let prev = this.head;
+       while(prev.next) {
+        prev = prev.next;
+       }
+       prev.next = node;
+    }
+    this.size++
+  }
+
+  insert(value, index) {
+        if(index < 0 || index > this.size) {
+          return
+        }
+        if(index === 0) {
+          this.prepend(value);
+        } else {
+          const node  = new Node(value);
+          let prev = this.head;
+          for(let i = 0; i < index - 1; i++) {
+            prev = prev.next
+          }
+          node.next = prev.next;
+          prev.next = node;
+          this.size++;
+        }
+  }
+
+  print() {
+    if(this.isEmpty()) {
+      console.log('List is empty');
+    } else {
+      let curr = this.head;
+      let listValues = '';
+      while(curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValues);
+    }
+  }
+}
+
+
+const list = new LinkedList();
+
+console.log('List is empty? ', list.isEmpty());  // true
+console.log('List size', list.getSize());  // 0
+
+list.print() // List is empty
+
+list.insert(10, 0);
+list.print(); // 10
+
+list.insert(20, 0);
+list.print(); // 20 10
+
+list.insert(30, 1);
+list.print() // 20 30 10
+
+list.insert(40, 2);
+list.print(); // 20 30 40 10
+
+console.log(list.getSize()); // 4
