dcb561a76c4dea0f4786aff0722a63a5cc943179
Mayur Mandani <mmandani9@gmail.com>
2025-07-05 10:53:08 +0530

    JavascriptPuzzle344 changes are committed

---
+++ JavascriptPuzzle344.js
@@ -0,0 +1,8 @@
+/* Write a function Anagram Checker */
+
+function isAnagram(str1, str2) {
+  const normalize = str => str.replace(/\s/g, '').toLowerCase().split('').sort().join('');
+  return normalize(str1) === normalize(str2);
+}
+
+console.log(isAnagram("sIl e nt", "listen")); // true
