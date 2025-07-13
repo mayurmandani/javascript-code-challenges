cc931469e14b8f50d95fbe578320055e9bdb749f
Mayur Mandani <mmandani9@gmail.com>
2025-07-13 09:12:55 +0530

    JavascriptPuzzle352 changes are committed

---
+++ JavascriptPuzzle352.js
@@ -0,0 +1,7 @@
+/*  Array.from() method can be used to convert any iterable objects into arrays. */
+
+const str = "Mayur";
+
+const arr = Array.from(str);
+
+console.log(arr); // [ 'M', 'a', 'y', 'u', 'r' ]
