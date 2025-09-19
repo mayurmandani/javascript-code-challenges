3a20e7cd85c4c9ff2a3e1ce3f464f0e295eab0dd
Mayur Mandani <mmandani9@gmail.com>
2025-09-19 10:53:03 +0530

    JavascriptPuzzle420 changes are committed

---
+++ JavascriptPuzzle420.js
@@ -0,0 +1,9 @@
+/* What will be the output of the below code: */
+
+const user = { name: "Mayur", skills: ["JS", "React"]};
+
+const copy = user;
+
+copy.name = "Anurag";
+
+console.log(user.name);  // Anurag
