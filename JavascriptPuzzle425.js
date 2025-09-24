26d272059e5efefeb6caa2dfd0531d7354004e5e
Mayur Mandani <mmandani9@gmail.com>
2025-09-24 09:33:44 +0530

    JavascriptPuzzle425 changes are committed

---
+++ JavascriptPuzzle425.js
@@ -0,0 +1,23 @@
+/* What will be the output of the below code: */
+
+function fetchData() {
+  return new Promise(resolve => setTimeout(() => resolve('data'), 0));
+}
+
+const p = fetchData();
+
+p.then(res => console.log('fetch then', res));
+
+async function run() {
+  console.log('async start');
+  const res = await p;
+  console.log('after await', res);
+}
+
+run();
+
+/*  
+async start
+fetch then data 
+after await data
+*/
