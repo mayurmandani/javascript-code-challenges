6b90ac0e7c15fddcab7d5af91aed33e5101fa55d
Mayur Mandani <mmandani9@gmail.com>
2025-10-25 11:40:15 +0530

    JavascriptPuzzle456 changes are committed

---
+++ JavascriptPuzzle456.js
@@ -0,0 +1,5 @@
+/* What will be the output of the below code: */
+
+console.log(+true);  // Unary plus(+) converts true to number // 1
+
+console.log(!'Mayur'); // Non-empty strings are truthy → true =>  Negation(!) gives false // false
