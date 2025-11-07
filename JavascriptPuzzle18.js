796ee523a09bf6535295d7b8ac9d66b45e64ab7c
Mayur Mandani <mmandani9@gmail.com>
2025-11-07 10:10:47 +0530

    JavascriptPuzzle18 changes are updated

--- JavascriptPuzzle18.js
+++ JavascriptPuzzle18.js
@@ -1,16 +1,21 @@
-/* Write a program to convert input [1,2,3,4,5] to output want to be [5,1,4,2,3] */
+/* Write a rearrangeArray function is Javascript which takes input as arr [1,2,3,4,5] and return output as arr [5,1,4,2,3] */
 
-let input = [1,2,3,4,5];
+let arr = [1,2,3,4,5];
 
-function changeArr(input) {
-    let output = input;
-    output[0] = 5;
-    output[1] = 1;
-    output[2] = 4;
-    output[3] = 2;
-    output[4] = 3;
-    return output;
+function rearrangeArray(arr) {
+    const result = [];
+    const n = arr.length;
+    let left = 0;
+    let right = n - 1;
+
+    while (left <= right) {
+        if(right >= left) result.push(arr[right--]);
+        if(left <= right) result.push(arr[left++]);
+    }
+    return result;
 }
 
-console.log(changeArr(input));
+const output = rearrangeArray(arr);
+
+console.log(output);
 
