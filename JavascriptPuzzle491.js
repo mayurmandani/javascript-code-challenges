28438147724a02025922c6c35e849c6d7119f739
Mayur Mandani <mmandani9@gmail.com>
2025-11-29 09:53:22 +0530

    JavascriptPuzzle491 changes are committed

---
+++ JavascriptPuzzle491.js
@@ -0,0 +1,19 @@
+/* Implement Currying function */
+
+function curry(fn) {
+  return function curried(...args) {
+    if(args.length >= fn.length) {
+      return fn(...args);
+    } else {
+      return (...nextArgs) => curried(...args, ...nextArgs);
+    }
+  };
+}
+
+function add(a, b, c) {
+  return a + b + c;
+}
+
+const curriedAdd = curry(add);
+
+console.log(curriedAdd(1)(2)(3)); // 6
