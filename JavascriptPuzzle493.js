b4a503cd3ac1673cdddddce781064cab85d03baa
Mayur Mandani <mmandani9@gmail.com>
2025-12-01 15:18:41 +0530

    JavascriptPuzzle493 changes are committed

---
+++ JavascriptPuzzle493.js
@@ -0,0 +1,11 @@
+/* What will be the output of the following code? */
+
+console.log([1] + []); // 1
+
+console.log([] + {});  // [object Object]
+
+console.log({} + []); // [object Object]
+
+console.log(1 < 2 < 3); // true
+
+console.log(3 > 2 > 1); // false
