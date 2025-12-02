1dc650e1f93e4d6968dde40db3e2010e17e0bb0c
Mayur Mandani <mmandani9@gmail.com>
2025-12-02 14:28:39 +0530

    JavascriptPuzzle494 changes are committed

---
+++ JavascriptPuzzle494.js
@@ -0,0 +1,18 @@
+/* What will be the output of the following code? */
+
+Promise.resolve().then(() => console.log(1));
+queueMicrotask(() => console.log(2));
+setTimeout(() => console.log(3), 0);
+console.log(4);
+new Promise(() => console.log(5));
+(async () => console.log(6))();
+
+/*
+4 
+5
+6
+1
+2
+3
+
+*/
