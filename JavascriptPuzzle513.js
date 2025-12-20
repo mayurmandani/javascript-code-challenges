4043dd18cad199cbb08ef121b73e476aac5ca64d
Mayur Mandani <mmandani9@gmail.com>
2025-12-20 15:14:36 +0530

    JavascriptPuzzle513 changes are committed

---
+++ JavascriptPuzzle513.js
@@ -0,0 +1,7 @@
+/* What will be the output of the following code? */
+
+let array = [3, 2, 1];
+let sortedArray = array.sort();
+
+console.log(array); // [ 1, 2, 3 ]
+console.log(sortedArray); // [ 1, 2, 3 ]
