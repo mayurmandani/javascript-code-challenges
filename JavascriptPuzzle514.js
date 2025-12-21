1725f33c77e697900b7831e244e0c9ed4c10e228
Mayur Mandani <mmandani9@gmail.com>
2025-12-21 12:41:48 +0530

    JavascriptPuzzle514 changes are committed

---
+++ JavascriptPuzzle514.js
@@ -0,0 +1,7 @@
+/* What will be the output of the following code? */
+
+let array = [3,2,1];
+let sortedArray = array.sortCopy();
+
+console.log(array);  // Output : [3, 2, 1]
+console.log(sortedArray); // Output: [1, 2, 3]
