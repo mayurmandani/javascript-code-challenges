4941ef6941a47d0b011bc2573a2293f8150e7187
Mayur Mandani <mmandani9@gmail.com>
2025-12-22 12:28:43 +0530

    JavascriptPuzzle515 changes are committed

---
+++ JavascriptPuzzle515.js
@@ -0,0 +1,6 @@
+/* What will be the output of the following code? */
+
+let array = [1,2,3,2,1];
+let findLastIndex = array.findLastIndex(element => element === 1);
+
+console.log(findLastIndex); // Output: 4
