54cd1f07694788ac4cbe3ec77286359466d1ea9d
Mayur Mandani <mmandani9@gmail.com>
2025-12-27 11:03:49 +0530

    JavascriptPuzzle520 changes are committed

---
+++ JavascriptPuzzle520.js
@@ -0,0 +1,18 @@
+/* What will be the output of the following code? */
+
+const obj = {
+  a: 1,
+  b: 2,
+  c: {
+    a: 3,
+    b: 4,
+  },
+};
+
+const {
+  a,
+  b,
+  c: { a: nestedA },
+} = obj;
+
+console.log(a, b, nestedA); // 1 2 3
