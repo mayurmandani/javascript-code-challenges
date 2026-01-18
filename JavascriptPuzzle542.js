69c5beba12e9d01e6d26fb9127564b2fea5e0656
Mayur Mandani <mmandani9@gmail.com>
2026-01-18 08:55:39 +0530

    JavascriptPuzzle542 changes are committed

---
+++ JavascriptPuzzle542.js
@@ -0,0 +1,23 @@
+// What is the output of the following code?
+
+const songsList = [
+    { name: "aaaaaa..", artist: "A" },
+    { name: "cccccccc..", artist: "B" },
+    { name: "abababa..", artist: "C" },
+    { name: "dddddd..", artist: "A" },
+    { name: "eeeeee..", artist: "A" },
+    { name: "ffffff..", artist: "B" },
+    { name: "ggggg..", artist: "A" },
+    ];
+
+    const result = {};
+
+    for (let elem of songsList) {
+        if(result[elem.artist]) {
+            result[elem.artist] += 1;
+        } else {
+            result[elem.artist] = 1;
+        }
+    }
+
+    console.log(result);  // { A: 4, B: 2, C: 1 }
