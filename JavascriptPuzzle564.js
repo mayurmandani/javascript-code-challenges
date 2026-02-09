9154c037c82b059260949b4d47276830644912a3
Mayur Mandani <mmandani9@gmail.com>
2026-02-09 13:52:58 +0530

    JavascriptPuzzle564 changes are committed

---
+++ JavascriptPuzzle564.js
@@ -0,0 +1,17 @@
+// What is the output of the following code?
+
+Promise.resolve().then(() => console.log(1));
+queueMicrotask(() => console.log(2));
+setTimeout(() => console.log(3), 0);
+console.log(4);
+new Promise(() => console.log(5));
+(async () => console.log(6))();
+
+/* 
+4
+5
+6
+1
+2
+3
+*/
